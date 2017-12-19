02a8b9e0fc327facf3e7eee7f04c4c6dad153505
hut <hut@hut.pm>
2017-12-19 23:50:59 +0100

    added time constants

--- coms/all.py
+++ coms/all.py
@@ -2,3 +2,4 @@ from coms.freedomunits import *
 from coms.math import *
 from coms.physics import *
 from coms.si import *
+from coms.time import *
---
+++ coms/time.py
@@ -0,0 +1,9 @@
+second = 1
+minute = 60 * second
+hour = 60 * minute
+day = 24 * hour
+week = 7 * day
+year = 365.25 * day
+month = 30.44 * day
+
+kmh = hour / 1000.0  # converts m/s -> km/h
