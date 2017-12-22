a777286fdc0301a4c8b601b1afb16065b74b2b31
hut <hut@hut.pm>
2017-12-22 10:19:32 +0100

    rename coms.math -> coms.mathematics
    this avoids 'from math import pi' to fail on retarded python versions

--- coms/all.py
+++ coms/all.py
@@ -1,6 +1,6 @@
 from coms.basic import *
 from coms.freedomunits import *
-from coms.math import *
+from coms.mathematics import *
 from coms.physics import *
 from coms.si import *
 from coms.time import *
